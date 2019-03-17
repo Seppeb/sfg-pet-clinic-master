0c760a5dd34dcdcd9cc538ab0d81b89ffb647a71
Seppe Berghmans <seppeberghmans@gmail.com>
2019-03-17 12:38:24 +0100

    first commit

---
+++ src/main/java/framework/spring/sfgpetclinicmaster/SfgPetClinicMasterApplication.java
@@ -0,0 +1,13 @@
+package framework.spring.sfgpetclinicmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicMasterApplication.class, args);
+    }
+
+}
